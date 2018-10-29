612a9c3a5a01c93e2ae98fd9657c9f93b7d0ec16
Simple <43014320+TofekHamodii@users.noreply.github.com>
2018-10-29 16:37:39 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,81 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+
+client.on('ready', function() {
+    console.log(`i am ready ${client.user.username}`);
+});
+
+
+
+
+
+
+
+
+
+
+
+const developers = ["452379478618800129","",""]
+const adminprefix = "*";
+client.on('message', message => {
+    var argresult = message.content.split(` `).slice(1).join(' ');
+      if (!developers.includes(message.author.id)) return;
+      
+  if (message.content.startsWith(adminprefix + 'ply')) {
+    client.user.setGame(argresult);
+      message.channel.send(`**Now Playig   ${argresult}**`)
+  } else 
+     if (message.content === (adminprefix + "leaveserver")) {
+    message.guild.leave();        
+  } else  
+  if (message.content.startsWith(adminprefix + 'wat')) {
+  client.user.setActivity(argresult, {type:'WATCHING'});
+      message.channel.send(`**Now Watching   ${argresult}**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'lis')) {
+  client.user.setActivity(argresult , {type:'LISTENING'});
+      message.channel.send(`**Now Listening   ${argresult}**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'stream')) {
+    client.user.setGame(argresult, "https://www.twitch.tv/idk");
+      message.channel.send(`**Now Streaming   ${argresult}**`)
+  }
+  if (message.content.startsWith(adminprefix + 'setname')) {
+  client.user.setUsername(argresult).then
+      message.channel.send(`Changing The Name To ..**${argresult}** `)
+} else
+if (message.content.startsWith(adminprefix + 'setavatar')) {
+  client.user.setAvatar(argresult);
+    message.channel.send(`Changing The Avatar To :**${argresult}** `);
+}
+});
+
+function clean(text) {
+    if (typeof(text) === "string")
+      return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
+    else
+        return text;
+}
+
+var prefix = "*"
+client.on('message', async message => {
+                        let args = message.content.split(" ").slice(1).join(" ")
+if(message.content.startsWith(prefix + 'cc')) {
+    if(!args) return message.channel.send('Select a number');
+             if (!message.member.hasPermission('MANAGE_ROLES')) returnmessage.channel.sendMessage('Missing permissions `MANAGE_ROLES`');
+             await message.channel.send(`**Creating ${args} color**`).then(c => {
+                  setInterval(function(){})
+                    let count = 0;
+                    let ecount = 0;
+          for(let x = 1; x < `${parseInt(args)+1}`; x++){
+            message.guild.createRole({name:x,
+              color: 'RANDOM',
+              permissions: [1]})
+              }
+              c.edit(`**Created ${args} color**`)
+            })
+            }
+       });
+
+client.login(process.env.BOT_TOKEN);
